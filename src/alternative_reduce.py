d96f710d2cc483c7e79dd799e1ff27c80550da91
rachelHoman <rachelmarie001@aol.com>
2024-02-05 19:32:54 -0800

    cleaning up

--- src/alternative_reduce.py
+++ src/alternative_reduce.py
@@ -10,37 +10,66 @@ args = parser.parse_args()
 # imports
 import os
 import json
+import glob
+import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.dates as plt_dates
+from matplotlib.ticker import MultipleLocator
 from collections import Counter, defaultdict
+from datetime import datetime
 
-# load each of the input paths
-total_hashtags = defaultdict(lambda: Counter())
+# initialize
+# total_hashtags = defaultdict(lambda: Counter())
+file_path = "/home/rmha2020/twitter_coronavirus/outputs/geoTwitter20*.lang"
+files = glob.glob(file_path)
+total = defaultdict(int)
 
-# scan through all the data in the outputs folder
-for path in os.listdir('outputs'):
-    if path.endswith('.json'):
-        with open(os.path.join('outputs', path)) as f:
-            tmp = json.load(f)
-            for k in tmp:
-                if k in args.hashtags:
-                    total_hashtags[k] += tmp[k]
+# go through files
+for file in files:
+    with open(file) as f:
+        date = os.path.basename(file)[10:18]
+        tmp = json.load(f)
+        for hashtag in args.hashtags:
+            if hashtag in tmp:
+                for language in tmp[hashtag]:
+                    if (date, hashtag) not in total:
+                        total[(date, hashtag)] = 0
+                total[(date, hashtag)] += np.sum(list(tmp[hashtag].values()))
 
 # plot the data
+total = dict(sorted(total.items(), key=lambda key: key[0]))
+fig, ax = plt.subplots()
+
 for hashtag in args.hashtags:
-    data = total_hashtags[hashtag]
-    x_values = list(data.keys())
-    y_values = list(data.values())
+    dates = []
+    counts = []
 
-    plt.plot(x_values, y_values, label=hashtag)
+    for k in total:
+        if k[1] == hashtag:
+            dates.append(k[0])
+            counts.append(total[k])
 
-# Customize the plot
-plt.xlabel('Day of the Year')
+    if dates:  # Check if the list is not empty
+        dates_list = [datetime.strptime(date_str, "%y-%m-%d") for date_str in dates]
+        ax.plot(dates_list, counts, label=hashtag)
+
+ax.xaxis.set_major_locator(plt_dates.MonthLocator(interval=2))
+ax.xaxis.set_major_formatter(plt_dates.DateFormatter("%y-%m-%d"))
+
+# add labels
+plt.xlabel('Date')
 plt.ylabel('Number of Tweets')
-plt.title('Number of Tweets with Hashtags Over the Year')
-#plt.legend()
-plt.grid(True)
+plt.title('Number of Daily Tweets By Hashtags')
+plt.legend()
+
+#finalHashes = ""
+#for hsh in args.hashtags:
+#    finalHashes += hsh
+
 
 # Save the plot to the specified output path
-plt.savefig(args.output_path)
+output_filename = f'{hashtag}_alt_reduce3.png'
+output_p = os.path.join(args.output_path, output_filename)
+plt.savefig(output_p)
